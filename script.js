b62fb1a432caa72cbcf11719edfbbe35c9eb61cb
programmingsquid <avaandelijah22@gmail.com>
2024-10-29 20:46:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function answerYes() {
     document.getElementById("que").style.display = "none";
     document.getElementById("init").innerText = "Here's the truth";
   document.getElementById("message").style.display = "block";
-  document.getElementById("messageText").innerText = "I really enjoyed going to the preds game with you and not getting taken away by a strange man in a hat whos had to much to drink. but i would be down to get a drink sometime.";
+  document.getElementById("messageText").innerText = "I really enjoyed going to the preds game with you and not getting taken away by a strange man in a hat whos had to much to drink. But i would be down to get a drink sometime.";
   showConfetti()
 }
 
